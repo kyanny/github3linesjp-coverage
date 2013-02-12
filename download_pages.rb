0f02cb6c718d3880456119737fd738ac556c5f22
Kensuke Nagae <kyanny@gmail.com>
2013-02-12 19:05:44 +0900

    最後のページ番号も自動的に取得するようにした

--- download_pages.rb
+++ download_pages.rb
@@ -1,9 +1,20 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
+#
+# GitHub Blog から全エントリ (インデックスページ) の HTML をダウンロードする
+require 'nokogiri'
 
+# HTML 置き場
 Dir.mkdir('tmp') unless Dir.exists?('tmp')
-page = (1..86)                  # ページ増えたら変更する
+
+# ページングのリンクから一番古いページ番号を得る
+`wget https://github.com/blog -O ./tmp/latest.html`
+doc = Nokogiri::HTML(open('latest.html'))
+oldest_page = doc.search('.pagination a').sort_by{ |anchor|
+  anchor['href'].match(/page=(\d+)/)[1].to_i # /blog?page=86 とかの 86 でソート
+}.last['href'].match(/page=(\d+)/)[1].to_i   # 一番最後のページの番号 86 を得る
+
 base_url = 'https://github.com/blog?page=%d'
-page.each do |i|
+(1..oldest_page).each do |i|
   `wget #{base_url % i} -O ./tmp/page#{i}.html`
 end
