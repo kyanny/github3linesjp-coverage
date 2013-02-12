ec76527ebdc4cee5c2ed4b8a1f91fe070b53f59d
Kensuke Nagae <kyanny@gmail.com>
2013-02-12 22:37:24 +0900

    Fix title

--- github_blog_metadata.rb
+++ github_blog_metadata.rb
@@ -5,14 +5,14 @@ require 'nokogiri'
 
 url = ARGV.shift
 doc = Nokogiri::HTML(open(url))
+title = doc.search('title').text
 doc.search('.blog-post').map do |post|
-  title     = post.search('.entry-title').first.text
   href      = post.search('.entry-title').first['href']
   published = post.search('.published').first['title']
   puts <<META
 #{href.sub(/\/blog\//, '')}
 
-出典: [#{title} - GitHub Blog](https://github.com#{href})
+出典: [#{title}](https://github.com#{href})
 
 #{published}
 META
