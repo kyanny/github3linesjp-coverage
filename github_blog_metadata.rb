8c6a970be8e8ad751207f2301a76c1ef12f6bfd2
Kensuke Nagae <kyanny@gmail.com>
2013-02-14 22:06:19 +0900

    Extract image urls except emoticons

--- github_blog_metadata.rb
+++ github_blog_metadata.rb
@@ -9,8 +9,18 @@ title = doc.search('title').text
 doc.search('.blog-post').map do |post|
   href      = post.search('.entry-title').first['href']
   published = post.search('.published').first['title']
+
+  entry_content = post.search('.entry-content').first
+  images = entry_content.search('img').map{ |img|
+    img['src']
+  } || []
+  images.reject!{ |img| img.match(%r!/images/icons/emoji/!) }
+  images = images.map{ |img| "![図](#{img})"}.join("\n")
+
   puts <<META
 #{href.sub(/\/blog\//, '')}
+
+#{images}
 
 出典: [#{title}](https://github.com#{href})
 
